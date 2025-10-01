4cc753270848979e1e46d4596437944972d13899
gitmvfs <marcossantos8002@gmail.com>
2025-10-01 20:39:28 -0300

    feat(database): CRUD features
    The principle ideia is abstract database layer from the rest of application, because if after we decide scale this application only update database class for mysql server, postgree, graphql, or another... Keeping modules and updating logical part.

---
+++ repositories/database.py
@@ -0,0 +1,34 @@
+import sqlite3
+import traceback
+
+class Database():
+
+    default_file_path = str('repositories/database.db')
+
+    def __init__(self, file_path:str = default_file_path):
+        self.connect(file_path)
+
+    def connect(self, file_path:str = default_file_path):
+
+        self.database = sqlite3.connect(file_path)
+        self.cursor = self.database.cursor() #implement actions in the database     
+
+    def update(self):
+        self.database.commit()
+    
+    def closeConnect(self):
+        self.cursor.close()
+        self.database.close()
+
+    def execute_query(self, query:str, parameters=''):
+        self.connect()
+        self.cursor.execute(query, parameters)
+        self.update()
+        self.closeConnect()
+
+    def execute_query_fetchall(self, query:str, parameters = '') -> list:
+        self.connect()
+        self.cursor.execute(query, parameters)
+        rows = self.cursor.fetchall()
+        self.closeConnect()
+        return rows
