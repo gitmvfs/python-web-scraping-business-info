7606ed0be5a989c3b5fca4a670488a43418f309d
gitmvfs <marcossantos8002@gmail.com>
2025-10-02 19:21:45 -0300

    feat(jwt_token) verify expire date token

--- utils/jwt_token.py
+++ utils/jwt_token.py
@@ -1,10 +1,11 @@
-from jwt import encode, types
+from jwt import encode, decode
 import os
 from dotenv  import load_dotenv, find_dotenv
 from datetime import datetime, timedelta
 
 config = load_dotenv(find_dotenv())
 JWT_SECRET = os.getenv("JWT_SECRET")
+ALGORITHM = 'HS256'
 
 def create_jwt( uuid: str):
 
@@ -17,7 +18,15 @@ def create_jwt( uuid: str):
         'expire_at': str(expire_at)
     }
     print(payload)
-    token = encode(payload, JWT_SECRET, algorithm='HS256')
+    token = encode(payload, JWT_SECRET, algorithm= ALGORITHM)
 
     return token
 
+
+def verify_jwt(token: str):
+    token = decode(token, JWT_SECRET, algorithms= ALGORITHM)
+
+    if token['create_at'] > token['expire_at']:
+        return False
+    else:
+        return True
